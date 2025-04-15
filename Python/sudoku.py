10562588ea16589d76157b7a2f816906ade1f147
abusumon <abusumon1701@gmail.com>
2025-04-15 23:47:04 +0600

    Updated Sudoku solver

--- Python/sudoku.py
+++ Python/sudoku.py
@@ -10,73 +10,74 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+class SudokuSolver:
+    def __init__(self, board):
+        self.board = board
 
-def solve(bo):
-    find = find_empty(bo)
-    if not find:
-        return True
-    else:
+    def solve(self):
+        find = self.find_empty()
+        if not find:
+            return True
         row, col = find
 
-    for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+        for i in range(1, 10):
+            if self.valid(i, (row, col)):
+                self.board[row][col] = i
 
-            if solve(bo):
-                return True
+                if self.solve():
+                    return True
 
-            bo[row][col] = 0
+                self.board[row][col] = 0
 
-    return False
-
-
-def valid(bo, num, pos):
-    # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
-            return False
-
-    # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
-            return False
-
-    # Check box
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
-
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+        return False
+    def valid(self, num, pos):
+        # Check row
+        for i in range(len(self.board[0])):
+            if self.board[pos[0]][i] == num and pos[1] != i:
                 return False
 
-    return True
+        # Check column
+        for i in range(len(self.board)):
+            if self.board[i][pos[1]] == num and pos[0] != i:
+                return False
+
+        # Check box
+        box_x = pos[1] // 3
+        box_y = pos[0] // 3
+
+        for i in range(box_y * 3, box_y * 3 + 3):
+            for j in range(box_x * 3, box_x * 3 + 3):
+                if self.board[i][j] == num and (i, j) != pos:
+                    return False
+
+        return True
 
 
-def print_board(bo):
-    for i in range(len(bo)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
+    def print_board(self):
+        for i in range(len(self.board)):
+            if i % 3 == 0 and i != 0:
+                print("- - - - - - - - - - - - - ")
 
-        for j in range(len(bo[0])):
-            if j % 3 == 0 and j != 0:
-                print(" | ", end="")
+            for j in range(len(self.board[0])):
+                if j % 3 == 0 and j != 0:
+                    print(" | ", end="")
 
-            if j == 8:
-                print(bo[i][j])
-            else:
-                print(str(bo[i][j]) + " ", end="")
+                if j == 8:
+                    print(self.board[i][j])
+                else:
+                    print(str(self.board[i][j]) + " ", end="")
 
 
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
-                return (i, j)  # row, col
+    def find_empty(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[0])):
+                if self.board[i][j] == 0:
+                    return (i, j)  # row, col
 
-    return None
+        return None
 
-print_board(board)
-solve(board)
-print("___________________")
-print_board(board)
+solver = SudokuSolver(board)
+solver.print_board()
+solver.solve()
+print("_________________")
+solver.print_board()
